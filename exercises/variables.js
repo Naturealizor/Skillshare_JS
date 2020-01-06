bfd324c588aa5b0e2d0c2d951b7235e6edb2ac73
Naturealizor <31334963+Naturealizor@users.noreply.github.com>
2020-01-05 22:35:38 -0600

    more JS practice, and found the useful mixins page

--- exercises/variables.js
+++ exercises/variables.js
@@ -4,3 +4,36 @@
 
 // str = "The blue blanket is sitting on the desk"
 // console.log(str)
+
+//                                                  CONCATENATING STRINGS TO FORM A URL
+// website = "https:arkmont.com/";
+
+// new_string = "The quiCK brown FOW jumps of the  lazy dog";
+
+// new_string = "The quiCK brown FOW jumps of the  lazy dog";
+
+// new_string = new_string.toLowerCase()
+
+// url = new_string.replace(" ", " ")
+
+// url = new_string.replace("  ", " ")
+
+// url = url.split(" ")
+
+// url = url.join('-')
+// url = website + url
+
+// console.log(url)
+
+// //                               PRINTS OUT "https:arkmont.com/the-quick-brown-fow-jumps-of-the-lazy-dog"
+
+// What does this function do?
+function something() {
+    // Step 1
+    // ...
+    // Step 2
+    // ...
+
+    // Return an object
+    return {}
+}
